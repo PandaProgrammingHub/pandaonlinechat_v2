9e49b603ccdf5211d37f5b3005eb318aa439d75a
Santosh Kumar Panda <pandaprogramminghub@gmail.com>
2020-06-23 18:06:04 +0530

    fixed wall post iamge issue

--- public/JS/chat.js
+++ public/JS/chat.js
@@ -184,7 +184,7 @@ socket.on("wallstatusPost", function (user, msg, sent_on) {
         <div class="panel-body">
           <div class="media msg">
             <a class="pull-left" href="#">
-              <img class="media-object img-circle"  data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src=${profilePic}}></a>
+              <img class="media-object img-circle"  data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src=${profilePic}></a>
                 <div class="media-body"><small class="col-lg-10">
                   ${$.emoticons.replace(msg)} 
                   </small>
